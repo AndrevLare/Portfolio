bda7a11f975a63553db7e5e477e74dfa3e865308
Andrés Oviedo <jorgitoa0109@gmail.com>
2024-04-18 18:48:09 -0500

    projects

--- js/desktopNav.js
+++ js/desktopNav.js
@@ -5,6 +5,7 @@ let mediaQuery = matchMedia("(min-width: 1000px) and (orientation: landscape)");
 const sections = [
   document.querySelector(".main__about"),
   document.querySelector(".main__contact"),
+  document.querySelector(".main__projects"),
 ];
 const navElements = [
   document.querySelector(".navElement1"),
@@ -18,6 +19,7 @@ changeSection = (section) => {
     case 0:
       sections[0].removeAttribute("hidden");
       sections[1].setAttribute("hidden", " ");
+      sections[2].setAttribute("hidden", " ");
 
       navElements[0].classList.add("header__nav-desktop__ul--li-Selected");
       navElements[1].classList.remove("header__nav-desktop__ul--li-Selected");
@@ -26,6 +28,7 @@ changeSection = (section) => {
     case 1:
       sections[1].removeAttribute("hidden");
       sections[0].setAttribute("hidden", " ");
+      sections[2].setAttribute("hidden", " ");
 
       navElements[1].classList.add("header__nav-desktop__ul--li-Selected");
       navElements[0].classList.remove("header__nav-desktop__ul--li-Selected");
@@ -34,6 +37,7 @@ changeSection = (section) => {
     case 2:
       sections[1].setAttribute("hidden", " ");
       sections[0].setAttribute("hidden", " ");
+      sections[2].removeAttribute("hidden");
 
       navElements[2].classList.add("header__nav-desktop__ul--li-Selected");
       navElements[1].classList.remove("header__nav-desktop__ul--li-Selected");
