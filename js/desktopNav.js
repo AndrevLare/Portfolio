661e2070750c36f7781f106b885f7959bcb88632
Andrés Oviedo <jorgitoa0109@gmail.com>
2024-08-09 23:33:06 -0500

    quitar formulario de contacto

--- js/desktopNav.js
+++ js/desktopNav.js
@@ -4,12 +4,12 @@ let mediaQuery = matchMedia("(min-width: 1000px) and (orientation: landscape)");
 //Codigo
 const sections = [
   document.querySelector(".main__about"),
-  document.querySelector(".main__contact"),
+  //document.querySelector(".main__contact"),
   document.querySelector(".main__projects"),
 ];
 const navElements = [
   document.querySelector(".navElement1"),
-  document.querySelector(".navElement2"),
+  //document.querySelector(".navElement2"),
   document.querySelector(".navElement3"),
 ];
 let currentSection = 0;
@@ -19,27 +19,18 @@ changeSection = (section) => {
     case 0:
       sections[0].removeAttribute("hidden");
       sections[1].setAttribute("hidden", " ");
-      sections[2].setAttribute("hidden", " ");
+      //sections[2].setAttribute("hidden", " ");
 
       navElements[0].classList.add("header__nav-desktop__ul--li-Selected");
       navElements[1].classList.remove("header__nav-desktop__ul--li-Selected");
-      navElements[2].classList.remove("header__nav-desktop__ul--li-Selected");
+      //navElements[2].classList.remove("header__nav-desktop__ul--li-Selected");
       break;
     case 1:
       sections[1].removeAttribute("hidden");
       sections[0].setAttribute("hidden", " ");
-      sections[2].setAttribute("hidden", " ");
+      //sections[2].removeAttribute("hidden");
 
-      navElements[1].classList.add("header__nav-desktop__ul--li-Selected");
-      navElements[0].classList.remove("header__nav-desktop__ul--li-Selected");
-      navElements[2].classList.remove("header__nav-desktop__ul--li-Selected");
-      break;
-    case 2:
-      sections[1].setAttribute("hidden", " ");
-      sections[0].setAttribute("hidden", " ");
-      sections[2].removeAttribute("hidden");
-
-      navElements[2].classList.add("header__nav-desktop__ul--li-Selected");
+      //navElements[2].classList.add("header__nav-desktop__ul--li-Selected");
       navElements[1].classList.remove("header__nav-desktop__ul--li-Selected");
       navElements[0].classList.remove("header__nav-desktop__ul--li-Selected");
       break;
