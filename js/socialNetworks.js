1d593d6a650e85e06e3ceb94498d416df2e0e36c
Andrés Oviedo <jorgitoa0109@gmail.com>
2024-09-19 21:36:31 -0500

    Update socialNetworks.js

--- js/socialNetworks.js
+++ js/socialNetworks.js
@@ -2,7 +2,7 @@ const links = [
   "http://wa.me/573197914026?text=Hola,%20buen%20d%C3%ADa",
   "https://www.instagram.com/andrevlare/",
   "https://x.com/Gorje_2022",
-  "https://bit.ly/3w3DVKb",
+  "https://bit.ly/3XQ4QnG",
   "https://www.facebook.com/people/AndrevLare/61565446117582/",
 ];
 
