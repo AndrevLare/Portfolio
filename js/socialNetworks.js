7ccab66fae8aeb1c79f406239ee9a3373233a70d
Andrés Oviedo <90996615+AndrevLare@users.noreply.github.com>
2025-11-19 12:46:27 -0500

    Update WhatsApp link in socialNetworks.js

--- js/socialNetworks.js
+++ js/socialNetworks.js
@@ -1,9 +1,8 @@
 const links = [
-  "http://wa.me/573197914026?text=Hola,%20buen%20d%C3%ADa",
+  "http://wa.me/573502453823?text=Hola,%20buen%20d%C3%ADa",
   "https://www.instagram.com/andrevlare/",
   "https://x.com/Gorje_2022",
   "https://bit.ly/3XQ4QnG",
-  "https://www.facebook.com/people/AndrevLare/61565446117582/",
 ];
 
 openSocialNetwork = (social) => {
